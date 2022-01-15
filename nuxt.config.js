b5fc9b322951631faef6e5ab9ee13c478a25fafc
chipsnet <doronomori660@outlook.jp>
2022-01-15 09:51:43 +0900

    🚧 とりあえずモデリング途中

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,10 +2,6 @@ import theme from "@nuxt/content-theme-docs";
 
 export default theme({
     loading: { color: "#48bb78" },
-    generate: {
-        fallback: "404.html", // for Netlify
-        routes: ["/"], // give the first url to start crawling
-    },
     i18n: {
         locales: () => [
             {
@@ -13,6 +9,12 @@ export default theme({
                 iso: "ja-JP",
                 file: "ja-JP.js",
                 name: "日本語",
+            },
+            {
+                code: "en",
+                iso: "en-US",
+                file: "en-US.js",
+                name: "English"
             }
         ],
         defaultLocale: "ja",
